aa41530c29c182359c123c310b008f3afb36f40f
Harrison <harrison3000@users.noreply.github.com>
2026-05-08 23:47:32 -0300

    just a bunch of things

--- scripts/datizeitor_tabajara.mjs
+++ scripts/datizeitor_tabajara.mjs
@@ -1,47 +1,14 @@
 //@ts-check
 
-import { readFile, writeFile } from 'node:fs/promises';
+import { readFile, open } from 'node:fs/promises';
+import { findAddressesInASM, parseKnown } from './dtz_utils.mjs';
 
 const dados = await readFile("memdumps/data.bin");
-const usedAddresses = (await readFile("reasm/maincode.asm"))
-    .toString("utf8")
-    .split("\n")
-    .map(function(s){
-        if(!s.includes("[0x")){
-            return null;
-        }
+const discovered = findAddressesInASM("reasm/maincode.asm");
+const known = parseKnown("reasm/known_vars.txt");
 
-        let x = /(q?word|byte) +\[0x([a-f0-9]{1,4})\]/.exec(s);
-        if(x !== null){
-            return {type : x[1], addr: x[2], addrNum: parseInt(x[2], 16), s};
-        }
-        //for some reason ghidra ommits the size for AX
-        let y = /( E?A[XL],)?\[0x([a-f0-9]{1,4})\](,E?A[XL])?$/.exec(s);
-        if(y !== null && (y[1] || y[3])){
-            let siz = "word";
-            if(y[0].includes("EAX")){
-                siz = "dword";
-            }else if(y[0].includes("AL")){
-                siz = "byte";
-            }
-            return {type: siz, addr: y[2], addrNum: parseInt(y[2], 16), s};
-        }
 
-        return null;        
-    })
-    .filter(x => x !== null)
-    .sort((a,b) => a.addrNum - b.addrNum)
-    .filter(function(v, i, a){
-        //remove dups
-        if(i === 0){
-            return true;
-        }
-        var ant = a[i-1];
-        return v.addrNum !== ant.addrNum;
-    })
-    ;
-
-/** @type {('z'|'u'|'s'|'d')[]} */
+/** @type {('z'|'u'|'s'|'d'|'Z')[]} */
 const catg = [...dados].map(function(v){
     if(v === 0){
         return "z"; //zero
@@ -49,177 +16,108 @@ const catg = [...dados].map(function(v){
     return "u"; //unknown
 });
 
-/** @type {Item[]} */
-let coisas = [];
 
-class Item {
-    inicio = 0;
-    len = 0;
-    getval = function(){};
-    comment = "";
-}
 
-const known = {};
-
-(await readFile("reasm/known_vars.txt"))
-    .toString("utf8")
-    .split("\n")
-    .filter(x => x.trim() !== "")
-    .forEach(s => {
-        let vals = s.split(",").map(s => s.trim());
-        known[vals[0]] = vals[2];
-        //TODO adicionar no used address tambem
-        debugger;
-    })
-
-function emmitStrings(idx, num){    
-    for(let i = 0; i < num; i++){
-        let inicio = idx;
-        let len = dados.subarray(idx).findIndex(x => x == 0);
-        let sl = dados.toString("ascii", idx, idx + len);
-
-        len++; //+1 because zero terminator
-        idx += len;
-
-        let it = new Item;
-        it.inicio = inicio;
-        it.len = len;
-        it.getval = () => `db "${sl}", 0`;
-
-        coisas.push(it);
-        catg.fill("s", inicio, inicio + len);
-    }
-}
-
-function findRun(ch, callback){
-    var total = catg.length;
-    var idx = 0;
-    while(idx < total){
-        if(catg[idx] != ch){
-            idx++;
-            continue;
-        }
-
-        let inicio = idx;
-        let len = catg.slice(idx).findIndex(x => x != ch);
-        if(len == -1){
-            //chegou no final
-            len = total - idx;
-        }
-
-        idx += len;
-
-        let it = new Item;
-        it.inicio = inicio;
-        it.len = len;
-
-        callback(it);
-        coisas.push(it);
-    }
-}
-
-function zeroRuns(){
-    findRun("z",
-        function(it){
-            let len = it.len;
-            it.comment = "unknown zeros";
-            it.getval = () => `resb ${len}`;
-        }
-    )
-}
-
-function unkRuns(){
-    findRun("u", function(it){
-        let d = dados.slice(it.inicio, it.inicio + it.len);
-        var u = [[]];
-        d.forEach(x => {
-            let last = u.at(-1);
-            last.push("0x" + x.toString(16));
-            if(last.length > 100){
-                u.push([]);
-            }
-        })
-
-        var i = u.map(x => "db " + x.join(", "));
-
-        it.getval = () => i.join("\n    ");
-        it.comment = "unknown data";
-        //debugger;
-    });
-}
-
-function processData(){
-    const sizes = {
-        dword: [4, "dd", "readUint32LE"],
-        word: [2, "dw", "readUint16LE"],
-        byte: [1, "db", "readUint8"],
-    }
-
-    for(let ad of usedAddresses){
-        const [len, opcode, func] = sizes[ad.type];
-        const adn = ad.addrNum;
-        const adnl = adn + len;
-        const theCat = catg.slice(adn, adnl).join("");
-        if(/[^zu]/.test(theCat)){
-            console.warn("address of already categorized data: " + ad.addr);
-            continue
-        }
-
-        catg.fill("d", adn, adnl);
-        let d = "";
-        if(/^z+$/.test(theCat)){
-            d = `resb ${len}`;
-        }else{
-            let u = dados.slice(adn, adnl);
-            let v = u[func]().toString(16);          
-            d = `${opcode} 0x${v}`;
-        }
-
-        let it = new Item;
-        it.inicio = adn;
-        it.len = len;
-        it.getval = () => d;
-
-        coisas.push(it);
-    }
-}
 
 //=========================================================================
 
-emmitStrings(0, 2);
-emmitStrings(0x1a03, 7);
-emmitStrings(0x5b01, 5);
-emmitStrings(0x5b38, 5);
+let aldevars = known.concat(discovered);
+for(let v of aldevars){
+    //TODO pointer finder
+    let a = v.addrNum;
+    if(v.type === "string"){
+        const e = dados.indexOf(0, a);
+        const st = dados.toString("ascii", a, e);
 
-processData();
-
-zeroRuns();
-unkRuns();
-
-
-//TODO pointers e coisas assim
-
-coisas.sort((a,b) => a.inicio - b.inicio );
-
-var st = [";data segment at home:" , ""];
-
-coisas.forEach(x => {
-    var a = x.inicio.toString(16);
-    var  k = known[a];
-    var c = x.comment;
-    if(k){
-        st.push(k + ":");
-        c = "current address: 0x" + a;
-    }else{
-        st.push(`dt_addr_${a}:`);
+        v.valor = `db "${st}", 0`;
+        v.skip = st.length + 1;
+        continue;
     }
-    if(c){
-        st.push("    ;"+ c);
+    if(v.type === "ptr"){
+        const val = dados.readUint16LE(a);
+        const nn = aldevars.find(x => x.addrNum === val);
+        if(nn){
+            v.valor = "dw " + nn.name;
+        }else{
+            v.valor = `dw 0x${val.toString(16)} ; pointer to unknown var`;
+        }
+
+        v.skip = 2;
+        continue;
     }
-    st.push("    " + x.getval());
+    if(v.type === "word"){
+        const val = dados.readUint16LE(a);
+        v.valor = val === 0 ? "resb 2" : "dw 0x" + val.toString(16) ;
+        v.skip = 2;
+        continue;
+    }
+    if(v.type === "dword"){
+        const val = dados.readUint32LE(a);
+        v.valor = val === 0 ? "resb 4" : "dd 0x" + val.toString(16) ;
+        v.skip = 4;
+        continue;
+    }
+    if(v.type === "byte"){
+        v.valor = "db 0x" + dados[a].toString(16);
+        v.skip = 1;
+        continue;
+    }
 
-    st.push("");
-})
+    v.skip = 1;
+}
 
-var uu = st.join("\n");
-await writeFile("reasm/dataseg.asm", uu);
+for(let v of aldevars){
+    const a = v.addrNum
+    catg.fill("d", a, a+v.skip);
+}
+
+const zeroruns = [];
+for(let mt of catg.join("").matchAll(/z{3,}/g)){
+    let r = {idx: mt.index, len: mt[0].length};
+    zeroruns.push(r);
+    catg.fill("Z",r.idx, r.idx+r.len)
+}
+
+const undefruns = [];
+for(let mt of catg.join("").matchAll(/[zu]{1,32}/g)){
+    undefruns.push({idx: mt.index, len: mt[0].length});
+}
+
+const out = await open("reasm/dataseg.asm", "w");
+await out.write(";data segment at home:\n")
+
+for(let i = 0; i < dados.length; ){
+    aldevars = aldevars.filter(x => x.addrNum >= i); // this is so I can see the values vanishing when debugging, not a optimization
+    const v = aldevars.find(x => x.addrNum === i);
+    if(!v){
+        let zr = zeroruns.find(x => x.idx === i);
+        if(zr){
+            await out.write("\n    resb " + zr.len + "\n");
+            i += zr.len;
+            continue;
+        }
+
+        let ur = undefruns.find(x => x.idx == i);
+        let l = ur?.len ?? 1;
+        let zp = [];
+        for(let j = 0;j < l;j++){
+            zp.push(`0x${dados[i].toString(16)}`);
+            i++;
+        }
+
+        await out.write("\n    ;unknown data\n    db " + zp.join(", ") + "\n");
+        continue
+    }
+
+    let sai = `
+${v.name}:
+    ${v.valor}
+`;
+
+    i += v.skip;    
+
+    await out.write(sai);
+
+}
+
+await out.close();
---
+++ scripts/dtz_utils.mjs
@@ -0,0 +1,84 @@
+import { readFileSync, writeFile } from 'node:fs';
+
+/**
+ * 
+ * @param {string} arq File name
+ * @returns {string[]}
+ */
+function file2lines(arq){
+    const l = readFileSync(arq, "utf-8").split("\n");
+    return l;
+}
+
+
+export function findAddressesInASM(filename){
+    const l = file2lines(filename);
+    const addrs = l.map(function(s){
+        if(!s.includes("[0x")){
+            return null;
+        }
+
+        let x = /(q?word|byte) +\[0x([a-f0-9]{1,4})\]/.exec(s);
+        if(x !== null){
+            return {type : x[1], addr: x[2], addrNum: parseInt(x[2], 16), s};
+        }
+        //for some reason ghidra ommits the size for AX
+        let y = /( E?A[XL],)?\[0x([a-f0-9]{1,4})\](,E?A[XL])?$/.exec(s);
+        if(y !== null && (y[1] || y[3])){
+            let siz = "word";
+            if(y[0].includes("EAX")){
+                siz = "dword";
+            }else if(y[0].includes("AL")){
+                siz = "byte";
+            }
+            return {type: siz, addr: y[2], addrNum: parseInt(y[2], 16), s};
+        }
+
+        return null;
+    })
+    .filter(x => x !== null)
+    .sort((a,b) => a.addrNum - b.addrNum)
+    .filter(function(v, i, a){
+        //remove dups
+        if(i === 0){
+            return true;
+        }
+        var ant = a[i-1];
+        return v.addrNum !== ant.addrNum;
+    }).map(function(u) {
+        return {
+            addr: u.addr,
+            type: u.type,
+            name: "dt_addr_" + u.addr,
+            addrNum: u.addrNum,
+            valor: "",
+            skip: 0,
+        };
+    });
+
+    return addrs;
+}
+
+export function parseKnown(filename){
+    const l = file2lines(filename);
+
+    const addrs = l.filter(x => {
+        x = x.trim();
+        if(x.startsWith(";")){
+            return false;
+        }
+        return x !== "";
+    }).map(s => {
+        let vals = s.split(",").map(s => s.trim());
+        return {
+            addr: vals[0],
+            type: vals[1],
+            name: vals[2],
+            addrNum: parseInt(vals[0], 16),
+            valor: "",
+            skip: 0,
+        };
+    })
+
+    return addrs;
+}
