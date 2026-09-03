a48bf58f92f043eaded023a531d63e70279dcc50
Harrison <harrison3000@users.noreply.github.com>
2026-09-03 14:49:05 -0300

    bunch of nitpicks

--- scripts/trintedolizator.mjs
+++ scripts/trintedolizator.mjs
@@ -9,6 +9,11 @@ const fixed = codigos
     .replaceAll("call far ","call ")
     .replaceAll(/^ +(STOS|LODS|MOVS|REP ST|XLAT)/gm, "    a16 $1")
     .replaceAll(/^ +(LOOP|JCXZ)  /gm, "    L_$1")
+    .replaceAll("[CS:BX +", "[CS:EBX * 2 +")
+    .replace("AND BX, 15", "AND EBX, 15")
+    .replace("AND BX, 63", "AND EBX, 63")
+    .replaceAll(/dw( +)\./g,"dd$1.")
+    .replaceAll("ADD         SP,0x2", "ADD         ESP,0x2")
     ;
 
 
